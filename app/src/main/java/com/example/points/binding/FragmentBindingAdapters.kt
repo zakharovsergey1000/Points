237178048132e9cd042e0224034544786da6a38e
Sergey <zakharovsergey1000@gmail.com>
2021-08-29 11:32:53 +0300

    added annotation OpenForTesting

--- app/src/main/java/com/example/points/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/com/example/points/binding/FragmentBindingAdapters.kt
@@ -22,12 +22,13 @@ import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestListener
+import com.example.points.testing.OpenForTesting
 import javax.inject.Inject
 
 /**
  * Binding adapters that work with a fragment instance.
  */
-//@OpenForTesting
+@OpenForTesting
 class FragmentBindingAdapters @Inject constructor(val fragment: Fragment) {
     @BindingAdapter(value = ["imageUrl", "imageRequestListener"], requireAll = false)
     fun bindImage(imageView: ImageView, url: String?, listener: RequestListener<Drawable?>?) {

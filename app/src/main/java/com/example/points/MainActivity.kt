8c466e2d78ff7e38a7b4b96123f439a465cba848
Sergey <zakharovsergey1000@gmail.com>
2021-09-03 22:08:08 +0300

    activity_main renamed to main_activity

--- app/src/main/java/com/example/points/MainActivity.kt
+++ app/src/main/java/com/example/points/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
     }
 
     override fun supportFragmentInjector() = dispatchingAndroidInjector

6b85ade217fe3d92ffbf064a5b5284720af855ba
Sergey <zakharovsergey1000@gmail.com>
2021-09-13 12:16:09 +0300

    changed comment

--- app/src/main/java/com/example/points/api/GetPointsResponse.kt
+++ app/src/main/java/com/example/points/api/GetPointsResponse.kt
@@ -5,7 +5,7 @@ import com.example.points.vo.Point
 import com.google.gson.annotations.SerializedName
 
 /**
- * Simple object to hold repo search responses. This is different from the Entity in the database
+ * Simple object to hold points search responses. This is different from the Entity in the database
  * because we are keeping a search result in 1 row and denormalizing list of results into a single
  * column.
  */
